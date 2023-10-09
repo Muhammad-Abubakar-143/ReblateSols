5356ba166e96a6a6ea806d60809cc7cb3d0e5443
Muhammad Abubakar <m3abubakar@gmail.com>
2023-10-09 14:00:24 +0500

    adding typinf effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,8 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
-import {Typed} from 'react-typed'
+import Typed from 'react-typed'
+
 
 const Hero = () => {
   return (
@@ -11,18 +12,21 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-5 h-5 rounded-full bg-[#FCA311]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
-          </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            Welcome To
           </p>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Reblate Solutions 
+          </h1>
+          <p className={`${styles.heroSubText} mt-2  text-white-100`}>
+            Where we provide the services of
+          </p>
+            <Typed strings={["Web Development", "Graphic Designing", "Walmart Expertise"]} loop backSpeed={140} typeSpeed={120} className="text-[#FCA311] font-normal text-[20px]"/>
         </div>
         <ComputersCanvas />
       </div>
