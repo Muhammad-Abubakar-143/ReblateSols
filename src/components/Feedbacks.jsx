5588e32460e3117ab563367475cead06580a31be
Muhammad Abubakar <m3abubakar@gmail.com>
2023-10-30 16:03:53 +0500

    highly optimize

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
-import { testimonials } from "../constants";
+import { testimonials } from "../constants/testimonial";
 import { Link } from "react-router-dom";
 
 const FeedbackCard = ({
